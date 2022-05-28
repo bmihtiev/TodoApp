90d3754900b25e59a84513517349cb8d91c2f2e1
bmihtiev <bmihtiev@gmail.com>
2022-05-28 13:31:11 +0300

    add delete function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 const app = {
     input: document.querySelector('input[type="text"]'),
     button: document.querySelector('button'),
-    todoList: document.querySelector('.todo__list')
+    todoList: document.querySelector('.todo__list'),
+    btnDelete: document.querySelector('.fa-trash-can'),
+    total: document.querySelector('.todo__total')
 }
 
-function submitHandler(e) {
+showTodos();
+
+function createTodo(e) {
     e.preventDefault();
 
     const task = app.input.value;
@@ -17,9 +21,8 @@ function submitHandler(e) {
     const renderList = document.createElement('li');
 
     renderList.innerHTML = `
-        <span>${task}</span>
-        <span class="completed"></span>
-        <span class="delete"></span>
+        <span><input type="checkbox"> ${task}</span>
+        <i class="fa-solid fa-trash-can"></i>
     `;
 
     let getLocalStorage = localStorage.getItem('New todo');
@@ -34,10 +37,11 @@ function submitHandler(e) {
     localStorage.setItem('New todo', JSON.stringify(listArr));
 
     app.todoList.appendChild(renderList);
+    app.input.value = '';
 
+    showTodos();
 }
 
-showTodos();
 
 function showTodos() {
     let getLocalStorage = localStorage.getItem('New todo');
@@ -48,21 +52,38 @@ function showTodos() {
         listArr = JSON.parse(getLocalStorage)
     }
 
-    console.log(listArr);
-
     let newElement = '';
 
     listArr.forEach((element, index) => {
-        newElement += `<li data-id="${index}">${element} <span class="delete"><i class="fa-solid fa-trash-can"></i></span></li>`;
+        newElement += `<li><span><input type="checkbox"> ${element}</span> <i onclick="removeItem(${index})" class="fa-solid fa-trash-can"></i></li>`;
     });
 
     app.todoList.innerHTML = newElement;
+
+    app.total.innerHTML = 'Total: ' + listArr.length
+
 }
 
-function removeTodo() {
-    
+app.todoList.addEventListener('change', function (e) {
+    if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox')
+        itemComplete();
+});
+
+function removeItem(index) {
+    let getLocalStorage = localStorage.getItem('New todo');
+
+    if (getLocalStorage === null) {
+        listArr = [];
+    } else {
+        listArr = JSON.parse(getLocalStorage)
+    }
+
+    listArr.splice(index, 1);
+    localStorage.setItem('New todo', JSON.stringify(listArr));
+    showTodos();
 }
+
 
 
 // Events
-app.button.addEventListener('click', submitHandler);
+app.button.addEventListener('click', createTodo);
